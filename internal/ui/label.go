fe8e5dedc92940194a49c47225e89a5722001e04
WaronLimsakul <poonro@gmail.com>
2026-01-01 12:43:00 -0500

    refactor: rename type int label ui

--- internal/ui/label.go
+++ internal/ui/label.go
@@ -8,64 +8,67 @@ import (
 	"gioui.org/widget/material"
 )
 
+type LabelStyle = material.LabelStyle
+type Theme = material.Theme
+
 // label components are supposed to be used as decorator pattern
 // Text() is a base component with LabelFunc type
-type LabelFunc = func(*material.Theme, *widget.Selectable, string) material.LabelStyle
+type LabelFunc = func(*Theme, *widget.Selectable, string) LabelStyle
 
 // Decorate any LabelStyle with LabelDecorator e.g. H1, H2, etc.
-type LabelDecorator = func(*material.Theme, material.LabelStyle) material.LabelStyle
+type LabelDecorator = func(*Theme, LabelStyle) LabelStyle
 
-func H1(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func H1(thm *Theme, label LabelStyle) LabelStyle {
 	label.TextSize = thm.TextSize * 96.0 / 16.0
 	label.Font.Weight = font.Bold
 	return label
 }
 
-func H2(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func H2(thm *Theme, label LabelStyle) LabelStyle {
 	label.TextSize = thm.TextSize * 60.0 / 16.0
 	label.Font.Weight = font.Bold
 	return label
 }
 
-func H3(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func H3(thm *Theme, label LabelStyle) LabelStyle {
 	label.TextSize = thm.TextSize * 48.0 / 16.0
 	label.Font.Weight = font.Bold
 	return label
 }
 
-func H4(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func H4(thm *Theme, label LabelStyle) LabelStyle {
 	label.TextSize = thm.TextSize * 34.0 / 16.0
 	label.Font.Weight = font.Bold
 	return label
 }
 
-func H5(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func H5(thm *Theme, label LabelStyle) LabelStyle {
 	label.TextSize = thm.TextSize * 24.0 / 16.0
 	label.Font.Weight = font.Bold
 	return label
 }
 
-func P(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func P(thm *Theme, label LabelStyle) LabelStyle {
 	label.TextSize = thm.TextSize
 	return label
 }
 
-func I(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func I(thm *Theme, label LabelStyle) LabelStyle {
 	label.Font.Style = font.Italic
 	return label
 }
 
-func B(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func B(thm *Theme, label LabelStyle) LabelStyle {
 	label.Font.Weight = font.Bold
 	return label
 }
 
-func A(thm *material.Theme, label material.LabelStyle) material.LabelStyle {
+func A(thm *Theme, label LabelStyle) LabelStyle {
 	label.Color = color.NRGBA{R: 0, G: 0, B: 238, A: 255}
 	return label
 }
 
-func Text(thm *material.Theme, selectable *widget.Selectable, txt string) material.LabelStyle {
+func Text(thm *Theme, selectable *widget.Selectable, txt string) LabelStyle {
 	text := material.Label(thm, thm.TextSize, txt)
 	text.State = selectable
 	return text
